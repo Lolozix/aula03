4ddf74ac6b82f07a800159937600b7e09f8b1c84
Lolozix <lorranasilvadamasceno@gmail.com>
2024-10-29 21:01:28 -0400

    commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+import ListaProdutos from "../pages/listaProdutos";
+
+export default function Home() {
+    const [produtos, setProdutos] = useState([]);
+
+    useEffect(() => {
+        const receberListaProdutos = async () => {
+            try {
+                const resposta = await fetch('https://fakestoreapi.com/products');
+                const listaProdutos = await resposta.json();
+                setProdutos(listaProdutos);
+            } 
+            catch (erro) {
+                console.error("Erro ao receber produtos:", erro);
+                alert("Ocorreu um erro ao receber os produtos");
+            }
+        };
+        receberListaProdutos();
+    }, []);
+
+    return (
+        <>
+            <ListaProdutos produtos={produtos} />
+        </>
+    );
+}
