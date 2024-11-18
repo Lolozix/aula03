87e1c47eb35bd29c6799277f902e138b440982c2
Lolozix <lorranasilvadamasceno@gmail.com>
2024-11-18 14:05:28 -0400

    commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,37 @@
 import { useState, useEffect } from "react";
-import ListaProdutos from "../pages/listaProdutos";
+import ListaProdutos from "styles/listaProdutos.module.css";
 
-export default function Home() {
-    const [produtos, setProdutos] = useState([]);
+export default function App() {
+    
+    const [lista, setLista] = useState([]);
 
     useEffect(() => {
         const receberListaProdutos = async () => {
             try {
                 const resposta = await fetch('https://fakestoreapi.com/products');
-                const listaProdutos = await resposta.json();
-                setProdutos(listaProdutos);
-            }catch (erro) {
-                alert("Ocorreu um erro ao receber os produtos");
+                const dados = awaitresposta.json();
+                setLista(dados);
+            } catch {
+                alert('Ocorreu um erro na comunicação com o servidor!');
             }
-        };
+        }
         receberListaProdutos();
     }, []);
 
+    if (lista.length === 0) {
+        return <h1>Caregando...</h1>
+    }
+
+    const ordenAz = () => {
+        const listaOrdenada = [...lista].sort((a, b) => a.title.localeCompare(b.title));
+        setLista(listaOrdenada);
+    }
+
     return (
         <>
-            <ListaProdutos produtos={produtos} />
+            <h1>Lista de Produtos</h1>
+            <button> onClick={() => ordenAz()}</button>
+            <ListaProdutos produtos={lista} />
         </>
     );
 }
